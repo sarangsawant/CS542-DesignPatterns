081edd67eab02db5748340213ad692a57f513723
Sarang Sawant <ssawant5@binghamton.edu>
2018-04-21 20:03:03 -0400

    deleting old repo

--- generic_checkpointing_objects/myArrayList/src/myArrayList/util/FileDisplayInterface.java
+++
@@ -1,8 +0,0 @@
-package myArrayList.util;
-
-public interface FileDisplayInterface {
-
-	//This method is used to write the output to file of the implemented functions
-	public void writeToFile(String s);
-	
-}
--- generic_checkpointing_objects/myArrayList/src/myArrayList/util/StdoutDisplayInterface.java
+++
@@ -1,6 +0,0 @@
-package myArrayList.util;
-
-public interface StdoutDisplayInterface {
-
-	public void writeToStdout(String s);
-}
